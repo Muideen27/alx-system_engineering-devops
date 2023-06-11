234e07555eff4b1e8d999867c6252c9ce75f5219
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-11 13:54:15 +0000

    debugging Apache error 500 with strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fix typo in settings of wordpress
+exec { 'fix setting':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
