dfb281f9c897d4f17fcf81e7b52e715b51da2a5d
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-18 20:27:14 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
-# fix our stack so that there is not failed requests
+# increases the nginx limit
 
-exec { 'change nginx limit':
-	command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
-        provider => shell,
+exec { 'fix file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
